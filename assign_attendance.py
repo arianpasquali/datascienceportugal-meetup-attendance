44a826524f73580dee04b3f5adcf7f3c6191d24f
PMMAraujo <pedro10araujo@gmail.com>
2020-03-06 18:37:46 +0000

    added function to add first timmers to the sheet

--- assign_attendance.py
+++ assign_attendance.py
@@ -5,7 +5,6 @@ import re
 import utils
 import logging
 
-import pandas as pd
 import click
 
 logging.basicConfig(level=logging.INFO)
@@ -33,13 +32,17 @@ def process_presences(dspt_spreadsheet_filepath, meetup_attendees_filepath):
 
     # check each member attendance
     members_df["attended"] = members_df["meetup_user_id"].apply(lambda x: isAttendee(x, attendees_df)) 
-
+   
     # select columns
     columns = ["ordem", "meetup_user_id", "name", "attended"]
     members_df = members_df[columns]
+    
+    # add people that this meetups was their first time in DSPT meetups
+    missing_df = first_timmers(members_df, attendees_df, columns)
+    members_df = pd.concat([members_df, missing_df], ignore_index=True)
+
     # debug
     # members_df.to_csv("presencas.csv", index=False)
-
     click.echo("Persisting attendance data into DSPT spreadsheet {}" .format( dspt_spreadsheet_filepath))
     tmpSheet = xls.wb.create_sheet(title="TempPresenças")
     for column_id, column_item  in enumerate(columns):
@@ -58,6 +61,27 @@ def isAttendee(x, attendees_df):
         if(str(u_id) == str(x)): 
             return "X"
     return ""
+
+def first_timmers(members_df, attendees_df, columns):
+    missing_ids = [int(x) for x in attendees_df["meetup_user_id"].values.astype(str)
+    if x not in members_df["meetup_user_id"].values]
+    len_missing = len(missing_ids)
+
+    filter_missing = attendees_df["meetup_user_id"].apply(lambda x: True if x in missing_ids else False)
+    tmp_df = attendees_df[filter_missing].copy()
+
+    missing_df = pd.DataFrame()
+    last_ordem = members_df["ordem"].values[-1]
+    new_ordem = [x + int(last_ordem) for x in range(1, len_missing +1)]
+
+    missing_df['ordem'] = new_ordem
+    missing_df['meetup_user_id'] = tmp_df['meetup_user_id'].values.astype('int64')
+    missing_df['member_name'] = tmp_df['member_name'].values
+    missing_df['attended'] = ['X'] * len_missing
+
+    missing_df.columns = columns
+    return missing_df
+    
 
 if __name__ == "__main__":
     process_presences()
